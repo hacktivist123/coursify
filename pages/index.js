ba392ed991ce631b32f48413efffdd6268245d14
hacktivist123 <akintayoshedrack@gmail.com>
2021-05-18 04:11:19 +0100

    clean index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
 // import { getCourses } from '../utils/db'
 import { useUser } from '@auth0/nextjs-auth0'
 
-// const HomePage = ({ courses }) => {
-//   return (
-//     <div>
-//       <h1>Courses</h1>
-//       <pre>{JSON.stringify(courses, null, 2)}</pre>
-//     </div>
-//   )
-// }
-
-// export const getStaticProps = async () => {
-//   const data = await getCourses()
-
-//   return {
-//     props: {
-//       courses: JSON.parse(JSON.stringify(data)),
-//     },
-//   }
-// }
-
-const HomePage = () => {
+ const HomePage = () => {
   const { user, error, isLoading } = useUser()
   isLoading ? <div>Loading...</div> : null
   error ? <div>{error.message}</div> : null
