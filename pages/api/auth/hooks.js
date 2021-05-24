b4fb2dcd62199791bb3e7aff93b799480f5f815e
hacktivist123 <akintayoshedrack@gmail.com>
2021-05-24 16:34:26 +0100

    added auth0 rules for authentication

--- pages/api/auth/hooks.js
+++ pages/api/auth/hooks.js
@@ -1,6 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 module.exports = async (req, res) => {
-  const { email } = JSON.parse(req.body)
-  // create user in prisma
-  console.log('user created')
-  res.send({ received: true })
+  try {
+    const { email, secret } = JSON.parse(req.body)
+    if (secret === process.env.AUTH0_HOOK_SECRET) {
+      const user = await prisma.user.create({
+        data: { email },
+      })
+      console.log('created user')
+    } else {
+      console.log('You forgot to send me your secret!')
+    }
+  } catch (err) {
+    console.log(err)
+  } finally {
+    await prisma.$disconnect()
+    res.send({ received: true })
+  }
 }
