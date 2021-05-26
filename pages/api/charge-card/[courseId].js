129f21b1578505ccf067705a4a81be1495b066d6
hacktivist123 <akintayoshedrack@gmail.com>
2021-05-26 14:01:18 +0100

    implement auth for charging users that are authenticated

--- pages/api/charge-card/[courseId].js
+++ pages/api/charge-card/[courseId].js
@@ -1,4 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0'
+
+const prisma = new PrismaClient()
+
 module.exports = async (req, res) => {
   const { courseId } = req.query
-  res.send(`charging card for course ${courseId}`)
+  const {
+    user: { email },
+  } = getSession(req, res)
+
+  const course = prisma.course.findUnique({
+    where: {
+      id: parseInt(courseId),
+    },
+  })
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+  res.send(
+    `charging ${user.email} ${course.price} cents for course ${courseId}`
+  )
 }
